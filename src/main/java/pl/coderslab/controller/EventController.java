a33c04f4d0c963d18a43c453b0205285c369194d
pannorris <jakub.stefankiewicz@wp.pl>
2023-07-01 12:59:04 +0200

    Zmienione tworzenie i edycja schematów

--- src/main/java/pl/coderslab/controller/EventController.java
+++ src/main/java/pl/coderslab/controller/EventController.java
@@ -19,6 +19,12 @@ public class EventController {
     private final EventService eventService;
     private final TreeService treeService;
 
+    /**
+     * Show schemas: trees and events. Add, edit, delete tree and event.
+     *
+     * @param model : model to create attribute.
+     * @return show-schemas.jsp file with all list options.
+     */
     @GetMapping(path = "/list")
     String showSchemas(Model model) {
         model.addAttribute("tree", new Tree());
@@ -39,6 +45,13 @@ public class EventController {
         return "redirect:/schema/list";
     }
 
+    /**
+     * Add selected events to tree.
+     *
+     * @param id  tree`s id.
+     * @param model : model to create attribute.
+     * @return tree-add-event.jsp file with events to select.
+     */
     @GetMapping(path = "/tree/add_events/{id}")
     String addEventsToTree(@PathVariable("id") Long id, Model model) {
         model.addAttribute("tree", treeService.findById(id));
@@ -52,6 +65,13 @@ public class EventController {
         return "redirect:/schema/list";
     }
 
+    /**
+     * Edit selected tree.
+     *
+     * @param id : tree`s id.
+     * @param model : model to create attribute.
+     * @return edit-tree.jsp file with inputs to edit.
+     */
     @GetMapping(path = "/tree/edit/{id}")
     String editTree(@PathVariable("id") Long id, Model model) {
         model.addAttribute("treeToEdit", treeService.findById(id));
@@ -64,12 +84,25 @@ public class EventController {
         return "redirect:/schema/list";
     }
 
+    /**
+     * Delete selected tree.
+     *
+     * @param id : tree`s id.
+     * @return : redirect to schema list.
+     */
     @GetMapping(path = "/tree/delete/{id}")
     String deleteTree(@PathVariable("id") Long id) {
         treeService.delete(treeService.findById(id));
         return "redirect:/schema/list";
     }
 
+    /**
+     * Edit selected event.
+     *
+     * @param id : event`s id.
+     * @param model : model to create attribute.
+     * @return : edit-event.jsp file with inputs to edit.
+     */
     @GetMapping(path = "/event/edit/{id}")
     String editEvent(@PathVariable("id") Long id, Model model) {
         model.addAttribute("eventToEdit", eventService.findById(id));
@@ -82,12 +115,25 @@ public class EventController {
         return "redirect:/schema/list";
     }
 
+    /**
+     * Delete selected event.
+     *
+     * @param id : event`s id.
+     * @return : redirect to schema list.
+     */
     @GetMapping(path = "/event/delete/{id}")
     String deleteEvent(@PathVariable("id") Long id) {
         eventService.delete(eventService.findById(id));
         return "redirect:/schema/list";
     }
 
+    /**
+     * Adds dependencies to event- events that must be completed to complete selected event.
+     *
+     * @param id : event`s id to add dependencies.
+     * @param model : model to create attribute.
+     * @return add-dependencies.jsp file to select dependencies.
+     */
     @GetMapping(path = "/dependencies/add/{id}")
     String addDependencies(@PathVariable("id") Long id, Model model) {
         model.addAttribute("event", eventService.findById(id));
